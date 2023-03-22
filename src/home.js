22a13f6a75e7b0134af0192dafd43b7207c76e17
RohitGupta1237 <rohitgupta12371380@gmail.com>
2023-03-22 14:23:26 +0530

    new chnages made

--- src/home.js
+++ src/home.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./App.css";
+import Lottie from 'lottie-react';
+import firstgif from './firstgif.json'
+import influencergif2 from './influencergif2.json'
+import influencer3 from './influencer3.json'
+
 function Home() {
   const navigate = useNavigate();
 
@@ -14,10 +19,15 @@ function Home() {
   return (
     <div className="App">
       <nav>
+      <Lottie animationData={firstgif}/>
         <button className="button influencer-button" onClick={onClick}>
           Influencer
         </button>
+        <Lottie animationData={influencer3}/>
+
         <button className="button brand-button" onClick={onClick2}>Brand</button>
+        <Lottie animationData={influencergif2}/>
+
       </nav>
     </div>
   );
